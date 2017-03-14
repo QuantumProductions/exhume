a4cddb412659c7d9be15fe34eb2f0fe649a908bf
quantumpotato <quantumpotato@gmail.com>
2017-03-14 13:33:17 -0400

    wip

--- thing.erl
+++ thing.erl
@@ -1,10 +1,27 @@
 -module(thing).
 -compile(export_all).
 
-process(<<27>>) ->
+eraseLine() -> 
+  Cols = os:cmd("tput cols"),
+  eraseLine(Columns).
+eraseLine(0) -> ok;
+eraseLine(Columns) -> 
+  io:fwrite(" "),
+  eraseLine(Columns - 1).
+
+eraseScreen() ->
+  {ok, Rows} = io:rows(), 
+  eraseScreen(Rows).
+eraseScreen(0) -> ok;
+eraseScreen(Rows) ->
+  eraseLine(),
+  eraseScreen(Rows - 1).
+
+process(<<27>>) t->
   io:fwrite("Ch: ~w", [<<27>>]),
   exit(self(), kill);
 process(Ch) ->
+  eraseScreen(),
   io:fwrite("Ch: ~w", [Ch]),
   get_char().
 
