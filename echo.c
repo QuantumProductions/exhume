e785aef9717f60158eb27c5c575704b46721ea2a
quantumpotato <quantumpotato@gmail.com>
2017-03-15 20:40:25 -0400

    Getting string args

--- echo.c
+++ echo.c
@@ -7,18 +7,19 @@ int main(void){
   int *out;
   system ("/bin/stty raw");
   FILE *fp;
-  while((c=getchar())!= '.') {
+  // while((c=getchar())!= 27) {
     char path[1035];
     fp = popen("stty size", "r");
 
     while (fgets(path, sizeof(path)-1, fp) != NULL) {
       printf("%s", path);
       char command[100];
-      sprintf(command, "erl -noshell -s program main %s -s init halt", path);
+      sprintf(command, "erl -noshell -run program main %s -s init stop", path);
       system(command);
     }
+    system("clear");
     printf("\r\n");
-  }
+  // }
   system ("/bin/stty cooked");
   system ("clear");
   system ("echo ok\n");
--- program.erl
+++ program.erl
@@ -5,9 +5,24 @@ printFirst(Arg) when is_atom(Arg) ->
   io:format("Atom\n");
 printFirst(Arg) when is_number(Arg) ->
   io:format("Number\n");
-printFirst(_) ->
+printFirst(Arg) ->
+  io:format("Argument: ~p", [Arg]),
   io:format("Who knows!?").
 
-main([H | Args]) ->
-    io:format("Args: ~p\n", [Args]),
-    printFirst(H).
+main([H | [T]]) ->
+    % io:format("Args: ~p\n", [Args]),
+    printFirst(H),
+    printFirst(T),
+    io:setopts([{binary, true}]),
+    get_char().
+
+process(<<27>>) ->
+  io:fwrite("Ch: ~w", [<<27>>]),
+  exit(self(), kill);
+process(Ch) ->
+  io:fwrite("Ch: ~w", [Ch]),
+  get_char().
+
+get_char() ->
+    Ch = io:get_chars("p: ", 1),
+    process(Ch).
