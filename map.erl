04371253baf2245dee64bd958659b07f00267189
quantumpotato <quantumpotato@gmail.com>
2017-03-18 12:46:28 -0400

    Draw a star at specified coordinates from prepareRender

--- map.erl
+++ map.erl
@@ -5,6 +5,17 @@
 empty_map_test() ->
   [["   "], ["   "], ["   "]] = emptyMap(3, 3).
 
+prepareRender_test() ->
+  A = lists:flatten([["        "], 
+    ["        "], 
+    ["        "], 
+    ["        "], 
+    [" *      "], 
+    ["        "], 
+    ["        "], 
+    ["        "]]),
+  A = prepareRender([2], 8, 8, 5).
+
 processInput([X], _) when X < 80 ->
   [X + 1];
 processInput([_], _) ->
@@ -27,22 +38,25 @@ drawData([H | T], W, Hi, Output) ->
 
 emptyMap(W, H) -> drawData([], W, H, []).
 
-prepareRender([X], W, H) ->
+prepareRender([X], W, H, Y) ->
   EmptyMap = emptyMap(W, H),
-  Y = 5,
-  ExistingLine = lists:sublist(EmptyMap, Y),
-  LineWithX = lists:sublist(ExistingLine, X - 1) ++ ["*"] ++ lists:nthtail(X, ExistingLine),
-  MapWithX = lists:sublist(EmptyMap, Y - 1) ++ LineWithX ++ lists:nthtail(Y, EmptyMap),
-  LineBreakMap = lists_join:join(MapWithX, ["\r\n"]),
-  FlattenedMap = lists:flatten(LineBreakMap),
-  drawData(FlattenedMap, W, H).
+  [ExistingLine | []] = lists:nth(Y, EmptyMap),
+  Part1 = lists:sublist(ExistingLine, X - 1),
+  Part2 = lists:append(Part1, "*"),
+  Part3 = lists:append(Part2, lists:nthtail(X, ExistingLine)),
+  MapWithX = lists:sublist(EmptyMap, Y - 1) ++ [[Part3]] ++ lists:nthtail(Y, EmptyMap),
+  lists:flatten(MapWithX).
+  % FlattenedMap = lists:flatten(LineBreakMap),
+  % drawData(FlattenedMap, W, H).
 
 handle_call({input, Ch, W, H}, _, State) ->
+  Y = 5,
   State2 = processInput(State, Ch),
-  {reply, prepareRender(State2, W, H), State2};
+  {reply, prepareRender(State2, W, H, Y), State2};
 handle_call({tick, W, H}, _, State) ->
   State2 = processTick(State),
-  {reply, prepareRender(State2, W, H), State2}.
+  Y = 5,
+  {reply, prepareRender(State2, W, H, Y), State2}.
 
 init([]) ->
   {ok, [1]}.
